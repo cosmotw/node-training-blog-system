317d9d343cf81808955f7fb5174f03080063e106
Cosmo <shooliss@gmail.com>
2018-09-29 18:45:22 +0800

    Added users routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET users register page. */
+router.get('/register', function(req, res, next) {
+  res.send('Register page.');
+});
+
+/* GET users signin page. */
+router.get('/signin', function(req, res, next) {
+  res.send('Signin page.');
+});
+
+/* GET users signout page. */
+router.get('/signout', function(req, res, next) {
+  res.send('Signout page.');
+});
+
+/* GET users forget page. */
+router.get('/forget', function(req, res, next) {
+  res.send('Forget page.');
+});
+
+/* GET users profile page. */
+router.get('/profile', function(req, res, next) {
+  res.send('Profile page.');
+});
+
+/* GET users add article page. */
+router.get('/add_article', function(req, res, next) {
+  res.send('Article page.');
+});
+
+/* GET users article modify page. */
+router.get('/modify/:id', function(req, res, next) {
+  res.send('Article modify page.');
+});
+
+/* GET users guest message page. */
+router.get('/guest_message/:id', function(req, res, next) {
+  res.send('Guest message page.');
 });
 
 module.exports = router;
