fa9f7d8052c2801dc00401d1426e8e1c17c79606
Cosmo <shooliss@gmail.com>
2018-09-30 19:19:07 +0800

    Added apis routes

--- routes/apis.js
+++ routes/apis.js
@@ -1,4 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+/* DELETE article. */
+router.delete('/delete/:id', function(req, res, next) {
+  res.send('Delete article.');
+});
+
+/* POST user login. */
+router.post('/login', function(req, res, next) {
+  res.send('User login.');
+});
+
+/* POST add article. */
+router.post('/add', function(req, res, next) {
+  res.send('Add article.');
+});
+
+/* PUT update article. */
+router.put('/update/:id', function(req, res, next) {
+  res.send('Update article.');
+});
+
+/* POST add article comment. */
+router.post('/comment/:id', function(req, res, next) {
+  res.send('Add article comment.');
+});
+
 module.exports = router;
